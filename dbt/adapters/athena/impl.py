9042a2cf12ec249c5dfd207b2324968d75deae2a
Xin Wang <xwang@seek.com.au>
2021-11-24 21:35:13 +1100

    Revert format

--- dbt/adapters/athena/impl.py
+++ dbt/adapters/athena/impl.py
@@ -10,7 +10,6 @@ from dbt.adapters.athena import AthenaConnectionManager
 from dbt.adapters.athena.relation import AthenaRelation
 from dbt.logger import GLOBAL_LOGGER as logger
 
-
 class AthenaAdapter(SQLAdapter):
     ConnectionManager = AthenaConnectionManager
     Relation = AthenaRelation
@@ -24,12 +23,16 @@ class AthenaAdapter(SQLAdapter):
         return "string"
 
     @classmethod
-    def convert_number_type(cls, agate_table: agate.Table, col_idx: int) -> str:
+    def convert_number_type(
+        cls, agate_table: agate.Table, col_idx: int
+    ) -> str:
         decimals = agate_table.aggregate(agate.MaxPrecision(col_idx))
         return "double" if decimals else "integer"
 
     @classmethod
-    def convert_datetime_type(cls, agate_table: agate.Table, col_idx: int) -> str:
+    def convert_datetime_type(
+            cls, agate_table: agate.Table, col_idx: int
+    ) -> str:
         return "timestamp"
 
     @available
@@ -40,27 +43,24 @@ class AthenaAdapter(SQLAdapter):
         return f"{client.s3_staging_dir}tables/{str(uuid4())}/"
 
     @available
-    def clean_up_partitions(self, database_name: str, table_name: str, where_condition: str):
+    def clean_up_partitions(
+        self, database_name: str, table_name: str, where_condition: str
+    ):
         # Look up Glue partitions & clean up
         conn = self.connections.get_thread_connection()
         client = conn.handle
 
-        glue_client = boto3.client("glue", region_name=client.region_name)
-        s3_resource = boto3.resource("s3", region_name=client.region_name)
-
+        glue_client = boto3.client('glue', region_name=client.region_name)
+        s3_resource = boto3.resource('s3', region_name=client.region_name)
         partitions = glue_client.get_partitions(
             # CatalogId='123456789012', # Need to make this configurable if it is different from default AWS Account ID
             DatabaseName=database_name,
             TableName=table_name,
-            Expression=where_condition,
+            Expression=where_condition
         )
-        p = re.compile("s3://([^/]*)/(.*)")
+        p = re.compile('s3://([^/]*)/(.*)')
         for partition in partitions["Partitions"]:
-            logger.debug(
-                "Deleting objects for partition '{}' at '{}'",
-                partition["Values"],
-                partition["StorageDescriptor"]["Location"],
-            )
+            logger.debug("Deleting objects for partition '{}' at '{}'", partition["Values"], partition["StorageDescriptor"]["Location"])
             m = p.match(partition["StorageDescriptor"]["Location"])
             if m is not None:
                 bucket_name = m.group(1)
@@ -69,32 +69,30 @@ class AthenaAdapter(SQLAdapter):
                 s3_bucket.objects.filter(Prefix=prefix).delete()
 
     @available
-    def unique_temp_table_suffix(self, suffix_initial="__dbt_tmp", length=8):
-        return f"{suffix_initial}_{str(uuid4())[:length]}"
-
-    @available
-    def clean_up_table(self, database_name: str, table_name: str):
+    def clean_up_table(
+        self, database_name: str, table_name: str
+    ):
         # Look up Glue partitions & clean up
         conn = self.connections.get_thread_connection()
         client = conn.handle
-        glue_client = boto3.client("glue", region_name=client.region_name)
+        glue_client = boto3.client('glue', region_name=client.region_name)
         try:
-            table = glue_client.get_table(DatabaseName=database_name, Name=table_name)
+            table = glue_client.get_table(
+                DatabaseName=database_name,
+                Name=table_name
+            )
         except ClientError as e:
-            if e.response["Error"]["Code"] == "EntityNotFoundException":
+            if e.response['Error']['Code'] == 'EntityNotFoundException':
                 logger.debug("Table '{}' does not exists - Ignoring", table_name)
                 return
 
         if table is not None:
-            logger.debug(
-                "Deleting table data from'{}'",
-                table["Table"]["StorageDescriptor"]["Location"],
-            )
-            p = re.compile("s3://([^/]*)/(.*)")
+            logger.debug("Deleting table data from'{}'", table["Table"]["StorageDescriptor"]["Location"])
+            p = re.compile('s3://([^/]*)/(.*)')
             m = p.match(table["Table"]["StorageDescriptor"]["Location"])
             if m is not None:
                 bucket_name = m.group(1)
                 prefix = m.group(2)
-                s3_resource = boto3.resource("s3", region_name=client.region_name)
+                s3_resource = boto3.resource('s3', region_name=client.region_name)
                 s3_bucket = s3_resource.Bucket(bucket_name)
                 s3_bucket.objects.filter(Prefix=prefix).delete()
